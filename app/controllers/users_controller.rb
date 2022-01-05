ab33b0ad2bc3c0582cefb36a8abc3a280eb30120
keepitclassick <keepitclassick@gmail.com>
2022-01-05 16:26:51 -0500

    add has_secure_password and update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,21 @@ class UsersController < ApplicationController
     user = User.new(user_params)
   if user.save
     session[:user_id] = user.id
-    redirect_to '/'
+    redirect_to :root, notice: "User created successfully"
   else
-    redirect_to '/signup'
+    render :new
+  end
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
