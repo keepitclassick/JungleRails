67c20c701c126d220b84dec79a6099ba26fe5474
keepitclassick <keepitclassick@gmail.com>
2022-01-10 15:13:23 -0500

    add quantity validity test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,12 +24,20 @@ RSpec.describe Product, type: :model do
     it "is not valid without a name" do
       subject.name = nil
       expect(subject).to_not be_valid
-      expect(subject.errors.full_messages.first).to eq "Name can't be blank"
+      expect(subject.errors.full_messages).to include "Name can't be blank"
     end
+
     it "is not valid without a price" do
       subject.price_cents = nil
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages).to include "Price can't be blank"
     end
+
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Quantity can't be blank"
+    end
+    
   end
 end
