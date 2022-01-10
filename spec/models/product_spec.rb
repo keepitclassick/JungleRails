9da3343f44b0c7cbae613e75e1c3a5e1f978cf80
keepitclassick <keepitclassick@gmail.com>
2022-01-10 14:11:49 -0500

    Add test to see product is valid when created with valid input

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:category) {
+    Category.create(name: "testCategory")
+  }
+  subject {
+    category.products.create(
+      name: "New Product",
+      description: "Look at me I'm a new product!",
+      image: "testImage",
+      price_cents: 10000,
+      quantity: 23,
+    )
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+      expect(subject.errors.full_messages).to be_empty
+    end
   end
 end
