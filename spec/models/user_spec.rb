0e2af6da5da3e7e9300f6d5473b622922bdc5f61
keepitclassick <keepitclassick@gmail.com>
2022-01-10 16:04:02 -0500

    add test for password match on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
       expect(user.errors.full_messages).to be_empty
     end
+
+    it "is not valid when password and password_confirmation don't match" do
+      user = User.new(first_name: "John", last_name: "Wick", email: "justwickit@example.com", password: "continental", password_confirmation: "excomm")
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include ("Password confirmation doesn't match Password")
+    end
+
   end
 end
